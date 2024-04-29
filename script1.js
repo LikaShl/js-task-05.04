801a72ed9c25f8d63e9d082c5a98a70001a93364
Anzhelika <AnzhelikaShleiko@gmail.com>
2024-04-29 10:05:51 +0200

    add homework for 05.05

--- script1.js
+++ script1.js
@@ -8,3 +8,24 @@
 //   -go -> поїзд {name} везе {к-ть пасажирів} зі швидкістю {spped}
 //   -stop -> поїзд {name} зупинився. Швидкість {speed}
 //   -newPassenger -> збільшили к-ть пасажирів на {x}
+
+
+
+ function Train(name, speed, passenger){
+   this.name = name;
+   this.speed = speed;
+   this.passenger = passenger;
+}
+
+Train.prototype.go = function (){
+    console.log(`поїзд ${this.name} везе ${this.passenger} зі швидкістю ${this.speed}`)
+}
+Train.prototype.stop = function() {
+    console.log(`поїзд ${this.name} зупинився. Швидкість ${this.speed}`)
+}
+
+Train.prototype.newPassenger =  function(x) {
+    console.log(`збільшили к-ть пасажирів на ${x}`)
+}
+
+
--- script2.js
+++ script2.js
@@ -4,12 +4,57 @@
 // 1.2 Ф-я не приймає жодних аргументів, а необхідні налаштування повністю передаються через bind
 // 1.3 Ф-я приймає фразу для заголовка, об'єкт із налаштуваннями передаємо через .apply();
 // */
-   // let colors = {
-        // background: 'purple',
-        // color: 'white'
- // }
+// let colors = {
+// background: 'purple',
+// color: 'white'
+// }
 // fucntion myCall (color) {
-      // document.body.style.background = this.background;
-      // document.body.style.color = color;
- // }
-   // myCall.call (colors, 'red');
+// document.body.style.background = this.background;
+// document.body.style.color = color;
+// }
+// myCall.call (colors, 'red');
+
+// 1.1
+let colors = {
+  background: "purple",
+  color: "white",
+};
+
+function myCall(color) {
+  document.body.style.background = this.background;
+  document.body.style.color = color;
+}
+
+myCall.call(colors, "red");
+
+// 1.2
+let newColors = {
+  background: "blue",
+  color: "yellow",
+};
+
+function myBind() {
+  document.body.style.background = this.background;
+  document.body.style.color = this.color;
+}
+
+let bindFunc = myBind.bind(newColors);
+bindFunc();
+
+// 1.3
+function myApply(header, settings) {
+  document.body.style.background = settings.background;
+  document.body.style.color = settings.color;
+
+  let h1 = document.createElement("h1");
+  h1.textContent = header;
+  document.body.appendChild(h1);
+}
+
+let headerText = "I know how binding works in JS";
+let applySettings = {
+  background: "green",
+  color: "black",
+};
+
+myApply.apply(null, [headerText, applySettings]);
